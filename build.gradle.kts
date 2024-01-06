bc41a67a645620268b4932001263659f88f9bf1d
1fexd <PGFvy85nC@protonmail.com>
2024-01-06 22:21:57 +0100

    chore: Include dependencies in jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.9.22"
-    java
+    `java-library`
     `maven-publish`
     id("net.nemerosa.versioning") version "3.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "fe.clearurlkt"
@@ -13,10 +16,21 @@ repositories {
     maven(url = "https://jitpack.io")
 }
 
+val implementation: Configuration by configurations
+val shadowImplementation: Configuration by configurations.creating
+implementation.extendsFrom(shadowImplementation)
+
 dependencies {
-    implementation("com.gitlab.grrfe:gson-ext:11.0.0")
+    api(kotlin("stdlib"))
+
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.github.1fexd:uriparser:0.0.9")
+
+    shadowImplementation("com.gitlab.grrfe:gson-ext:11.0.0") {
+        isTransitive = false
+    }
+    shadowImplementation("com.github.1fexd:uriparser:0.0.9") {
+        isTransitive = false
+    }
 
     testImplementation(kotlin("test"))
 }
@@ -25,18 +39,48 @@ kotlin {
     jvmToolchain(17)
 }
 
+val shadowJarTask = tasks.named<ShadowJar>("shadowJar") {
+    mergeServiceFiles()
+    exclude("META-INF/**/*")
+
+    listOf("fe.gson", "fe.uribuilder", "org.apache.hc.core5").forEach { pkg ->
+        relocate(pkg, "fe.clearurlskt.internal.$pkg")
+    }
+
+    archiveClassifier.set("")
+    minimize()
+    configurations = listOf(shadowImplementation)
+}
+
+
+tasks.named("jar").configure {
+    enabled = false
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("shadow") {
+            setArtifacts(listOf(shadowJarTask.get()))
             groupId = project.group.toString()
             version = project.version.toString()
-
-            from(components["java"])
         }
     }
 }
 
+//configurations {
+//    artifacts {
+//        runtimeElements(shadowJarTask)
+//        apiElements(shadowJarTask)
+//    }
+//}
+
+tasks.whenTaskAdded {
+    if (name == "generateMetadataFileForPluginShadowPublication") {
+        println(name)
+        dependsOn(shadowJarTask)
+    }
+}
