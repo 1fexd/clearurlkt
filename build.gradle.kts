993f25d3aa93e139619bc267a74085b2aac67e61
1fexd <PGFvy85nC@protonmail.com>
2024-01-07 17:00:25 +0100

    chore: Include dependencies in jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.utils.IDEAPluginsCompatibilityAPI
+import org.jetbrains.kotlin.utils.addToStdlib.firstNotNullResult
+import java.net.URL
+
 plugins {
     kotlin("jvm") version "1.9.22"
-    java
+    `java-library`
     `maven-publish`
+    `project-report`
     id("net.nemerosa.versioning") version "3.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "fe.clearurlkt"
@@ -13,10 +20,16 @@ repositories {
     maven(url = "https://jitpack.io")
 }
 
+val implementation: Configuration by configurations
+val shadowImplementation: Configuration by configurations.creating
+implementation.extendsFrom(shadowImplementation)
+
 dependencies {
-    implementation("com.gitlab.grrfe:gson-ext:11.0.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.github.1fexd:uriparser:0.0.9")
+    api(kotlin("stdlib"))
+
+    shadowImplementation("com.google.code.gson:gson:2.+")
+    shadowImplementation("com.gitlab.grrfe.gson-ext:core:14.0.2-gson2-koin3")
+    shadowImplementation("com.github.1fexd:uriparser:0.0.10")
 
     testImplementation(kotlin("test"))
 }
@@ -25,18 +38,47 @@ kotlin {
     jvmToolchain(17)
 }
 
+val shadowJarTask = tasks.named<ShadowJar>("shadowJar") {
+    mergeServiceFiles()
+    exclude("META-INF/**/*")
+
+    listOf("fe.gson", "com.google.gson").forEach { pkg ->
+        relocate(pkg, "fe.clearurlskt.internal.$pkg")
+    }
+
+    archiveClassifier.set("")
+    minimize()
+    configurations = listOf(shadowImplementation)
+}
+
+
+tasks.named("jar").configure {
+    enabled = false
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("shadow") {
+            setArtifacts(listOf(shadowJarTask.get()))
             groupId = project.group.toString()
             version = project.version.toString()
-
-            from(components["java"])
         }
     }
 }
 
+//configurations {
+//    artifacts {
+//        runtimeElements(shadowJarTask)
+//        apiElements(shadowJarTask)
+//    }
+//}
+
+tasks.whenTaskAdded {
+    if (name == "generateMetadataFileForPluginShadowPublication") {
+        dependsOn(shadowJarTask)
+    }
+}
