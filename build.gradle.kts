b1d7dbe54334ca5846353a854fdf7c1910376328
1fexd <PGFvy85nC@protonmail.com>
2024-02-15 17:37:19 +0100

    chore: Bump uriparser

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     relocate("com.google.code.gson:gson:2.10.1")
     relocate("com.gitlab.grrfe:gson-ext:11.0.0")
 //    relocate("com.gitlab.grrfe.gson-ext:core:14.0.2-gson2-koin3")
-    relocate("com.github.1fexd:uriparser:0.0.10")
+    relocate("com.github.1fexd:uriparser:0.0.11")
 }
