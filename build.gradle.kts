57bed0eb1a025e4e6655e6450a0b8d1c974fdd8b
1fexd <PGFvy85nC@protonmail.com>
2024-06-22 19:13:37 +0200

    chore: Move to monorepo

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,12 @@ library("fe.clearurlkt") {
 }
 
 dependencies {
-    relocate("com.google.code.gson:gson:2.10.1")
-    relocate("com.gitlab.grrfe:gson-ext:11.0.0")
+    implementation("fe.gson.ext:core")
+    implementation("fe.uribuilder:uriparser")
+//    implementation("org.apache.httpcomponents.core5:httpcore5:5.3-alpha1")
+//    implementation(project(":uriparser"))
+//    relocate("com.google.code.gson:gson:2.10.1")
+//    relocate("com.gitlab.grrfe:gson-ext:11.0.0")
 //    relocate("com.gitlab.grrfe.gson-ext:core:14.0.2-gson2-koin3")
-    relocate("com.github.1fexd:uriparser:0.0.11")
+//    relocate("com.github.1fexd:uriparser:0.0.11")
 }
